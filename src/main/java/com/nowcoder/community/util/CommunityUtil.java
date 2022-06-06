dfb4e08fe2b8f8c108207a5fa8d4f7f40ba0c543
WangHui <wanghui.sd@outlook.com>
2022-06-06 17:11:41 +0800

    add registration function

---
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+public class CommunityUtil {
+
+    // 生成随机字符串
+    public static String generateUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    // MD5加密
+    // 自己设置的密码 + salt（我们给用户添加的） -> 加密结果
+    public static String md5(String key) {
+        if(StringUtils.isBlank(key)) {
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+
+}
