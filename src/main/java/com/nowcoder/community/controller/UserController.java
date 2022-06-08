eb71cb072632034f2466bf83cbd1f04efe0205ee
WangHui <wanghui.sd@outlook.com>
2022-06-08 21:13:59 +0800

    add set user count function and add check login status function

---
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -0,0 +1,114 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.annotation.LoginRequired;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    public static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @LoginRequired
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getSettingPage() {
+        return "/site/setting";
+    }
+
+    @LoginRequired
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model) {
+        if (headerImage == null) {
+            model.addAttribute("error", "您还没有选择图片！");
+            return "/site/setting";
+        }
+
+        String filename = headerImage.getOriginalFilename();
+        String suffix = filename.substring(filename.lastIndexOf("."));
+        if(StringUtils.isBlank(suffix)) {
+            model.addAttribute("error", "文件格式不正确！");
+            return "/site/setting";
+        }
+
+        // 生成随机文件名
+        filename = CommunityUtil.generateUUID() + suffix;
+        // 确定文件存放的路径
+        File dest = new File(uploadPath + "/" + filename);
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败：" + e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器发生异常！", e);
+        }
+
+        // 更新当前用户的头像的路径（web访问路径）
+        // http://localhost:8080/community/user/header/xxx.png
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + filename;
+        userService.updateHeader(user.getId(), headerUrl);
+
+        return "redirect:/index";
+    }
+
+    // 修改完图片后，每次访问index页面，MVC按照更改后的图片路径匹配controller方法，
+    // 而图片路径变为 http://localhost:8080/community/user/header/xxx.png 格式
+    // 因此寻找 user（当前类路径）下的header/xxx.jpg(当前方法)的getHeader方法
+    // 这个方法将电脑路径中保存的图片用byte数组传给了浏览器
+    @RequestMapping(path = "header/{filename}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("filename") String filename, HttpServletResponse response) {
+        // 服务器存放路径
+        filename = uploadPath + "/" + filename;
+        // 文件后缀
+        String suffix = filename.substring(filename.lastIndexOf(".") + 1);
+        // 响应图片
+        response.setContentType("image/" + suffix);
+        try (
+                OutputStream os = response.getOutputStream();
+                FileInputStream fis = new FileInputStream(filename);
+                ) {
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while((b = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败：" + e.getMessage());
+        }
+    }
+
+}
